0d9d1a54ffc7627bd8718672f8ba377df898b059
Allison Espinoza <alleespi@espol.edu.ec>
2020-08-31 14:30:00 -0500

    locales, scanner, camera

--- splashDemo/src/app/escanner/escanner.page.ts
+++ splashDemo/src/app/escanner/escanner.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from "src/app/services/auth.service";
 import { PhotoService } from '../services/camara.service';
-
+import {QRScanner} from 'cordova-plugin-qrscanner';
 
 
 @Component({
@@ -13,6 +13,32 @@ export class EscannerPage implements OnInit {
 
   constructor(private authservice:AuthService,public photoService: PhotoService) { }
 
+  public scanner(){
+
+    QRScanner.prepare(onDone); // show the prompt
+
+    function onDone(err, status){
+      if (err) {
+       // here we can handle errors and clean up any loose ends.
+       console.error(err);
+      }
+      if (status.authorized) {
+        // W00t, you have camera access and the scanner is initialized.
+        // QRscanner.show() should feel very fast.
+      } else if (status.denied) {
+       // The video preview will remain black, and scanning is disabled. We can
+       // try to ask the user to change their mind, but we'll have to send them
+       // to their device settings with `QRScanner.openSettings()`.
+      } else {
+        // we didn't get permission, but we didn't get permanently denied. (On
+        // Android, a denial isn't permanent unless the user checks the "Don't
+        // ask again" box.) We can ask again at the next relevant opportunity.
+      }
+    }
+  
+    QRScanner.scan();
+    QRScanner.show();
+  }
   ngOnInit() {
   }
   logout(){
--- splashDemo/src/app/folder/folder.page.ts
+++ splashDemo/src/app/folder/folder.page.ts
@@ -4,7 +4,7 @@ import { PhotoService } from '../services/photo.service';
 
 import { AuthService } from "src/app/services/auth.service";
 import { ActionSheetController } from '@ionic/angular';
-import {QRScanner} from 'cordova-plugin-qrscanner';
+
 
 @Component({
   selector: 'app-folder',
@@ -16,32 +16,7 @@ export class FolderPage implements OnInit {
 
   constructor(private activatedRoute: ActivatedRoute, private authservice:AuthService,public photoService: PhotoService) { }
 
-  public scanner(){
-
-    QRScanner.prepare(onDone); // show the prompt
-
-    function onDone(err, status){
-      if (err) {
-       // here we can handle errors and clean up any loose ends.
-       console.error(err);
-      }
-      if (status.authorized) {
-        // W00t, you have camera access and the scanner is initialized.
-        // QRscanner.show() should feel very fast.
-      } else if (status.denied) {
-       // The video preview will remain black, and scanning is disabled. We can
-       // try to ask the user to change their mind, but we'll have to send them
-       // to their device settings with `QRScanner.openSettings()`.
-      } else {
-        // we didn't get permission, but we didn't get permanently denied. (On
-        // Android, a denial isn't permanent unless the user checks the "Don't
-        // ask again" box.) We can ask again at the next relevant opportunity.
-      }
-    }
   
-    QRScanner.scan();
-    QRScanner.show();
-  }
   ngOnInit() {
     }
   logout(){
