5df6c8b7407a89daee44d315433985877c435144
Allison Espinoza <alleespi@espol.edu.ec>
2020-09-02 13:12:33 -0500

    scanner

--- splashDemo/src/app/escanner/escanner.page.ts
+++ splashDemo/src/app/escanner/escanner.page.ts
@@ -1,49 +1,158 @@
-import { Component, OnInit } from '@angular/core';
+
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { AuthService } from "src/app/services/auth.service";
 import { PhotoService } from '../services/camara.service';
-//import {QRScanner} from 'cordova-plugin-qrscanner';
-import { DocumentScanner} from '@ionic-native/document-scanner';
+import { ToastController, LoadingController, Platform } from '@ionic/angular';
+import jsQR from 'jsqr';
+
 
 @Component({
   selector: 'app-escanner',
   templateUrl: './escanner.page.html',
   styleUrls: ['./escanner.page.scss'],
 })
-export class EscannerPage implements OnInit {
+export class EscannerPage {
 
-  constructor(private authservice:AuthService,public photoService: PhotoService) { }
+  @ViewChild('video', { static: false }) video: ElementRef;
+  @ViewChild('canvas', { static: false }) canvas: ElementRef;
+  @ViewChild('fileinput', { static: false }) fileinput: ElementRef;
 
+  canvasElement: any;
+  videoElement: any;
+  canvasContext: any;
+  scanActive = false;
+  scanResult = null;
+  loading: HTMLIonLoadingElement = null
 
-  public documentscanner(){
-    DocumentScanner.scanDoc()
-  }
- /* public scanner(){
+  constructor(private authservice:AuthService,public photoService: PhotoService,
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController,
+    private plt: Platform) { 
 
-    QRScanner.prepare(onDone); // show the prompt
-
-    function onDone(err, status){
-      if (err) {
-       // here we can handle errors and clean up any loose ends.
-       console.error(err);
-      }
-      if (status.authorized) {
-        // W00t, you have camera access and the scanner is initialized.
-        // QRscanner.show() should feel very fast.
-      } else if (status.denied) {
-       // The video preview will remain black, and scanning is disabled. We can
-       // try to ask the user to change their mind, but we'll have to send them
-       // to their device settings with `QRScanner.openSettings()`.
-      } else {
-        // we didn't get permission, but we didn't get permanently denied. (On
-        // Android, a denial isn't permanent unless the user checks the "Don't
-        // ask again" box.) We can ask again at the next relevant opportunity.
-      }
+      const isInStandaloneMode = () =>
+      'standalone' in window.navigator && window.navigator['standalone'];
+    if (this.plt.is('ios') && isInStandaloneMode()) {
+      console.log('I am a an iOS PWA!');
+      // E.g. hide the scan functionality!
     }
-  
-    QRScanner.scan();
-    QRScanner.show();
-  }*/
-  ngOnInit() {
+    }
+ 
+  ngAfterViewInit() {
+    this.canvasElement = this.canvas.nativeElement;
+    this.canvasContext = this.canvasElement.getContext('2d');
+    this.videoElement = this.video.nativeElement;
+  }
+ 
+  // Helper functions
+  async showQrToast() {
+    const toast = await this.toastCtrl.create({
+      message: `Open ${this.scanResult}?`,
+      position: 'top',
+      buttons: [
+        {
+          text: 'Open',
+          handler: () => {
+            window.open(this.scanResult, '_system', 'location=yes');
+          }
+        }
+      ]
+    });
+    toast.present();
+  }
+ 
+  reset() {
+    this.scanResult = null;
+  }
+ 
+  stopScan() {
+    this.scanActive = false;
+  }
+
+  async startScan() {
+    // Not working on iOS standalone mode!
+    const stream = await navigator.mediaDevices.getUserMedia({
+      video: { facingMode: 'environment' }
+    });
+   
+    this.videoElement.srcObject = stream;
+    // Required for Safari
+    this.videoElement.setAttribute('playsinline', true);
+   
+    this.loading = await this.loadingCtrl.create({});
+    await this.loading.present();
+   
+    this.videoElement.play();
+    requestAnimationFrame(this.scan.bind(this));
+  }
+   
+  async scan() {
+    if (this.videoElement.readyState === this.videoElement.HAVE_ENOUGH_DATA) {
+      if (this.loading) {
+        await this.loading.dismiss();
+        this.loading = null;
+        this.scanActive = true;
+      }
+   
+      this.canvasElement.height = this.videoElement.videoHeight;
+      this.canvasElement.width = this.videoElement.videoWidth;
+   
+      this.canvasContext.drawImage(
+        this.videoElement,
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+   
+      if (code) {
+        this.scanActive = false;
+        this.scanResult = code.data;
+        this.showQrToast();
+      } else {
+        if (this.scanActive) {
+          requestAnimationFrame(this.scan.bind(this));
+        }
+      }
+    } else {
+      requestAnimationFrame(this.scan.bind(this));
+    }
+  }
+
+  captureImage() {
+    this.fileinput.nativeElement.click();
+  }
+   
+  handleFile(files: FileList) {
+    const file = files.item(0);
+   
+    var img = new Image();
+    img.onload = () => {
+      this.canvasContext.drawImage(img, 0, 0, this.canvasElement.width, this.canvasElement.height);
+      const imageData = this.canvasContext.getImageData(
+        0,
+        0,
+        this.canvasElement.width,
+        this.canvasElement.height
+      );
+      const code = jsQR(imageData.data, imageData.width, imageData.height, {
+        inversionAttempts: 'dontInvert'
+      });
+   
+      if (code) {
+        this.scanResult = code.data;
+        this.showQrToast();
+      }
+    };
+    img.src = URL.createObjectURL(file);
   }
   logout(){
     this.authservice.logout();
@@ -51,6 +160,7 @@ export class EscannerPage implements OnInit {
   addPhotoToGallery() {
     this.photoService.addNewToGallery();
   }
-
-
+  ngOnInit() {
+  }
+  
 }
